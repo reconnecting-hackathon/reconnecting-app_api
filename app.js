7c6cb09b2a15cf7a04553416992349d242336928
Joey Diperi <joey.diperi@C02QQAHKG8WP.local>
2018-05-21 22:33:43 -0400

    added cors info to app.js

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 //middleware
-app.use(cors({origin: '*'})) 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
