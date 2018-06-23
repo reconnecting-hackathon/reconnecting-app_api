5a911786dea4397f76d9017bbf9c9c54e216a09b
Joey Diperi <joey.diperi@C02QQAHKG8WP.local>
2018-06-23 13:54:46 -0400

    updated migration schema

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -1,26 +1,45 @@
 
-CREATE TABLE IF NOT EXISTS images
+CREATE TABLE IF NOT EXISTS user
 (
   id SERIAL PRIMARY KEY,
-  image_name VARCHAR(255) NOT NULL,
-  location_name VARCHAR(255) NOT NULL,
-  descriptions TEXT,
-  featured_image boolean,
-  vertical boolean
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  password_name VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS people
+CREATE TABLE IF NOT EXISTS contacts
 (
   id SERIAL PRIMARY KEY,
-  person_name VARCHAR(255) NOT NULL
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(255),
+  prefered_contact_method VARCHAR(255),
+  frequency VARCHAR(255),
 );
 
-CREATE TABLE IF NOT EXISTS image_person_join
+CREATE TABLE IF NOT EXISTS activities
 (
   id SERIAL PRIMARY KEY,
-  people_id INT REFERENCES people(id) NOT NULL,
-  image_id INT REFERENCES images(id) NOT NULL 
+  activity_name VARCHAR(255),
+  image_url VARCHAR(255) ,
+);
+
+CREATE TABLE IF NOT EXISTS activities_contacts_join
+(
+  id SERIAL PRIMARY KEY,
+  activity_id INT REFERENCES activities(id),
+  contacts_id INT REFERENCES contacts(id),
+);
+
+CREATE TABLE IF NOT EXISTS actions
+(
+  id SERIAL PRIMARY KEY,
+  action_complete boolean,
+  start_time date 
+  contacts_id INT REFERENCES contacts(id)
 );
 
 
-
+`
