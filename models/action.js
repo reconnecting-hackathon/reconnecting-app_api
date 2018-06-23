68a3a50d16e23b615a3f2b594badff458d83f092
copper-blue <copper-blue@users.noreply.github.com>
2018-06-23 16:28:15 -0400

    added actions model

---
+++ models/action.js
@@ -0,0 +1,23 @@
+const db = require('../db/config');
+
+const action = {};
+
+action.getActionsForUser = (user_id) => {
+  return db.query(
+    `SELECT a.id
+      , a.action_complete
+      , a.start_time
+    FROM actions a
+    INNER JOIN contacts c
+      ON c.id = a.contacts_id
+    WHERE c.user_id = $1`, [user_id]);
+};
+
+
+action.completeAction = (action_id, updated_start_time) => {
+  return db.query(`
+    UPDATE actions
+      SET start_time = $1
+    WHERE action_id = $2
+  `, [updated_start_time, action_id]);
+}
